f008cada0b2403341116f075794ffff19dc7da79
Asher Spurr <144856487+AsherSpurr@users.noreply.github.com>
2024-05-08 22:51:26 -0600

    Add styling to Service cards

--- src/components/subComps/outlineCard/OutlineCard.tsx
+++ src/components/subComps/outlineCard/OutlineCard.tsx
@@ -2,6 +2,7 @@ import {
   Card,
   CardContent,
   CardActions,
+  CardMedia,
   Typography,
   Grid,
   Box,
@@ -24,12 +25,64 @@ function OutlineCard() {
             return (
               <Grid item>
                 <Stack spacing={2}>
-                  <Card sx={{ height: 400, maxWidth: 345 }}>
-                    <CardContent>
-                      <Typography textAlign="center" variant="h2" sx={{ mt: 5 }}>
+                  <Card
+                    sx={{
+                      margin: "auto",
+                      borderRadius: 10,
+                      boxShadow: "0px 5px 10px rgba(34, 35, 58, 0.1)",
+                      position: "relative",
+                      maxWidth: 400,
+                      height: 300,
+                      marginLeft: "auto",
+                      overflow: "initial",
+                      background: "rgba(255, 146, 98, 0.03)",
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                      paddingBottom: 15,
+                    }}
+                  >
+                    <CardMedia
+                      sx={{
+                        width: "88%",
+                        marginLeft: "auto",
+                        marginRight: "auto",
+                        marginTop: -3,
+                        height: 0,
+                        paddingBottom: "48%",
+                        borderRadius: 2,
+                        backgroundColor: "#fff",
+                        position: "relative",
+                        "&:after": {
+                          content: '" "',
+                          position: "absolute",
+                          top: 0,
+                          left: 0,
+                          width: "100%",
+                          height: "100%",
+                          backgroundImage:
+                            "linear-gradient(147deg, #fe8a39 0%, #fd3838 74%)",
+                          borderRadius: 10,
+                          // 16
+                          opacity: 0.5,
+                        },
+                      }}
+                    >
+                      <Typography
+                      
+                        textAlign="center"
+                        variant="h2"
+                        sx={{ mt: 5 }}
+                      >
                         {service.name}
                       </Typography>
-                      <Typography textAlign="center" variant="body1" sx={{ mt: 5 }}>
+                    </CardMedia>
+                    <CardContent>
+                      <Typography
+                        textAlign="center"
+                        variant="body1"
+                        sx={{ mt: 5 }}
+                      >
                         {service.description}
                       </Typography>
                     </CardContent>
@@ -37,7 +90,7 @@ function OutlineCard() {
                   </Card>
                 </Stack>
               </Grid>
-            )
+            );
           })}
         </Grid>
       </Box>
