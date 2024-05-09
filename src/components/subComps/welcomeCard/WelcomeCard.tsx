53196da2144568a706b09d87ee7193c551f95e7c
Asher Spurr <144856487+AsherSpurr@users.noreply.github.com>
2024-05-08 23:02:07 -0600

    Add some fancy pants styling to Service cards

--- src/components/subComps/welcomeCard/WelcomeCard.tsx
+++ src/components/subComps/welcomeCard/WelcomeCard.tsx
@@ -1,11 +1,20 @@
-import { Card, CardContent, CardActions, Typography } from "@mui/material";
+import { Card, CardContent, CardActions, Typography, Stack } from "@mui/material";
 
 function WelcomeCard() {
   return (
     <div className="welcome-card">
-      <Card>
+      <Card
+        sx={{
+          margin: "auto",
+          borderRadius: 10,
+          maxWidth: 1200,
+          height: 600,
+          background: "rgba(255, 146, 98, 0.8)",
+        }}
+      >
         <CardContent>
-          <Typography variant="h2">Sup</Typography>
+          <Typography variant="h2">What you're going through is hard.</Typography>
+          <Typography variant="h4">I will make it harder</Typography>
         </CardContent>
         <CardActions></CardActions>
       </Card>
--- src/components/views/landing/Landing.tsx
+++ src/components/views/landing/Landing.tsx
@@ -1,12 +1,15 @@
 
 import WelcomeCard from "../../subComps/welcomeCard/WelcomeCard";
 import LandingCards from "../../subComps/landingArticles/LandingCards";
+import { Stack } from "@mui/material";
 
 function Landing() {
   return (
     <div>
+      <Stack spacing={25}>
       <WelcomeCard/>
       <LandingCards/>
+      </Stack>
     </div>
   );
 }
