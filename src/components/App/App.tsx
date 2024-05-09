e89a2656c6095b1f6a507a0b114c088d97f97464
Asher Spurr <144856487+AsherSpurr@users.noreply.github.com>
2024-05-09 13:54:13 -0600

    Add MainDashboard route; Education route

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Landing from '../views/landing/Landing';
 import CreateAccount from '../views/createAccount/CreateAccount';
+import Education from '../views/education/Education';
 import { Routes, Route, Link } from 'react-router-dom';
 import { useState } from 'react';
 
@@ -20,6 +21,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path='account/new' element={<CreateAccount/>}/>
+        <Route path='education' element={<Education />}/>
       </Routes>
     </div>
   );
