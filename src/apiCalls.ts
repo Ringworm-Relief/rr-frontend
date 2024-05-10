147b4d19f9dbc92cb1e537a9aa5ab5d64d4b1089
Asher Spurr <144856487+AsherSpurr@users.noreply.github.com>
2024-05-10 10:19:28 -0600

    Access single user by id; need to change to username + pass

--- src/apiCalls.ts
+++ src/apiCalls.ts
@@ -18,3 +18,18 @@ export const postNewUser = (newUser: NewUser) => {
     }
 })
 };
+
+// export const fetchUser = (email: string, password: string) => {
+//     return fetch(`https://8deefa6e-9aee-47e2-b8ea-a4dd591b3fc3.mock.pstmn.io/api/v1/users?email=${email}&password=${password}`)
+// }
+
+export const fetchUser = (userId: number) => {
+    return fetch(`https://8deefa6e-9aee-47e2-b8ea-a4dd591b3fc3.mock.pstmn.io/api/v1/users/${userId}`)
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error("Failed to fetch user.");
+        }
+    })
+}
