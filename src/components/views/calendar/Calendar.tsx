9ba80db0bf984be872425a8fc20c34e4cd3ad265
Asher Spurr <144856487+AsherSpurr@users.noreply.github.com>
2024-05-16 15:45:24 -0600

    Comment out - only use UI for now

--- src/components/views/calendar/Calendar.tsx
+++ src/components/views/calendar/Calendar.tsx
@@ -14,69 +14,69 @@ interface Props {
 function Calendar({ user }: Props) {
   const navigate = useNavigate();
   const scheduleObj = useRef<ScheduleComponent>(null);
-  // const storedEvents: any[] = JSON.parse(localStorage.getItem('events') || '[]');
-  const [events, setEvents] = useState<any[]>([]);
-  // console.log(scheduleObj)
-//undefined
-  // console.log(scheduleObj.current?.eventsData) //undefined
-  // console.log(scheduleObj.current?.activeCellsData)
-  // console.log(scheduleObj.current?.eventsProcessed)
+//   // const storedEvents: any[] = JSON.parse(localStorage.getItem('events') || '[]');
+//   const [events, setEvents] = useState<any[]>([]);
+//   // console.log(scheduleObj)
+// //undefined
+//   // console.log(scheduleObj.current?.eventsData) //undefined
+//   // console.log(scheduleObj.current?.activeCellsData)
+//   // console.log(scheduleObj.current?.eventsProcessed)
 
-  useEffect(() => {
-    const newNewEvent: any = {
-      dataSource: [{
-        Subject: 'Testing',
-        EndTime: new Date("2024-05-17T18:00:00.000Z"),
-        StartTime: new Date("2024-05-17T14:00:00.000Z"),
-        IsAllDay: false,
-        Description: 'Testing'
-      }]
-    }
-    setEvents([...events, newNewEvent])
-    }, [])
-    
-    // const eventSettings: EventSettingsModel = {dataSource: events}
+//   useEffect(() => {
+  //     setEvents([...events, newNewEvent])
+  //     }, [])
+  
+      const newNewEvent: any = {
+        dataSource: [{
+          Subject: 'Testing',
+          EndTime: new Date("2024-05-17T18:00:00.000Z"),
+          StartTime: new Date("2024-05-17T14:00:00.000Z"),
+          IsAllDay: false,
+          Description: 'Testing'
+        }]
+      }
+//     // const eventSettings: EventSettingsModel = {dataSource: events}
 
 
-  console.log(scheduleObj.current?.eventsData[0].StartTime)
-  useEffect(() => {
-    // scheduleObj.current?.addEvent({event: scheduleObj.current?.eventSettings.dataSource as any[] || []});
-    // setEvents(scheduleObj.current?.eventsData as any[] || [])
-    const subject = scheduleObj.current?.eventsData[0].Subject
-    const endTime = scheduleObj.current?.eventsData[0].EndTime
-    const startTime = scheduleObj.current?.eventsData[0].StartTime
-    const isAllDay = scheduleObj.current?.eventsData[0].IsAllDay  
-    const description = scheduleObj.current?.eventsData[0].Description
-    // let newEvent: EventSettingsModel = {
-    //   dataSource: [{
-    //     Subject: subject,
-    //     EndTime: new Date(`${endTime}`),
-    //     StartTime: new Date(`${startTime}`),
-    //     IsAllDay: isAllDay,
-    //     Description: description
-    //   }]
-    // }
-    let newEvent: any = {
-      dataSource: [{
-        Subject: subject,
-        EndTime: new Date(`${endTime}`),
-        StartTime: new Date(`${startTime}`),
-        IsAllDay: isAllDay,
-        Description: description
-      }]
-    }
-    setEvents(newEvent)
-    // console.log(scheduleObj.current?.eventsProcessed) //NOT undefined
-  }, [scheduleObj])
-  console.log('events', events)
-  console.log(scheduleObj)
+//   console.log(scheduleObj.current?.eventsData[0].StartTime)
+//   useEffect(() => {
+//     // scheduleObj.current?.addEvent({event: scheduleObj.current?.eventSettings.dataSource as any[] || []});
+//     // setEvents(scheduleObj.current?.eventsData as any[] || [])
+//     const subject = scheduleObj.current?.eventsData[0].Subject
+//     const endTime = scheduleObj.current?.eventsData[0].EndTime
+//     const startTime = scheduleObj.current?.eventsData[0].StartTime
+//     const isAllDay = scheduleObj.current?.eventsData[0].IsAllDay  
+//     const description = scheduleObj.current?.eventsData[0].Description
+//     // let newEvent: EventSettingsModel = {
+//     //   dataSource: [{
+//     //     Subject: subject,
+//     //     EndTime: new Date(`${endTime}`),
+//     //     StartTime: new Date(`${startTime}`),
+//     //     IsAllDay: isAllDay,
+//     //     Description: description
+//     //   }]
+//     // }
+//     let newEvent: any = {
+//       dataSource: [{
+//         Subject: subject,
+//         EndTime: new Date(`${endTime}`),
+//         StartTime: new Date(`${startTime}`),
+//         IsAllDay: isAllDay,
+//         Description: description
+//       }]
+//     }
+//     setEvents(newEvent)
+//     // console.log(scheduleObj.current?.eventsProcessed) //NOT undefined
+//   }, [scheduleObj])
+//   console.log('events', events)
+//   console.log(scheduleObj)
 
 
   return (
     <>
       {user.id ? (
         <>
-          <ScheduleComponent eventSettings={events} ref={scheduleObj}>
+          <ScheduleComponent eventSettings={newNewEvent} ref={scheduleObj}>
             <Inject services={[Day, Week, Month, Agenda]} />
           </ScheduleComponent>
         </>
