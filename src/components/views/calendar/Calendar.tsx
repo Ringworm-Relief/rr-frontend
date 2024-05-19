f060b79aeb17968851e054a0f01ecadc850c26db
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-05-19 00:18:50 -0700

    feat: Create API call for CalendarEvents, and start catch/try block for displaying CalendarEvents from API

--- src/components/views/calendar/Calendar.tsx
+++ src/components/views/calendar/Calendar.tsx
@@ -1,6 +1,4 @@
-import { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
-
 import { useEffect, useRef, useState, Dispatch, SetStateAction } from "react";
 import { ButtonComponent } from '@syncfusion/ej2-react-buttons';
 import { EventSettingsModel, ScheduleComponent, Day, Week, Agenda, Month, Inject, ViewsDirective, ViewDirective, PopupCloseEventArgs } from '@syncfusion/ej2-react-schedule';
@@ -8,7 +6,7 @@ import { DataManager, WebApiAdaptor, Query } from '@syncfusion/ej2-data';
 import {
   
 } from '@syncfusion/ej2-react-schedule';
-
+import { fetchCalendarEvents } from "../../../apiCalls/calendarApiCalls";
 interface Props {
   user: any;
 }
@@ -33,26 +31,39 @@ interface ApiEvent {
   };
 }
 
-const transformToScheduleData = (event: ApiEvent): ScheduleEvent => {
-  return {
-    Id: Number(event.id),
-    Subject: event.attributes.title,
-    Description: event.attributes.description,
-    StartTime: new Date(event.attributes.start_time),
-    EndTime: new Date(event.attributes.end_time),
-  };
-};
+const transformToApiFormat = (event: ScheduleEvent, user_id: number) => {
+  const startDate = event.StartTime.toLocaleDateString('en-GB', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit'
+  });
 
-const transformToApiFormat = (event: ScheduleEvent, userId: number) => {
+  const endDate = event.EndTime.toLocaleDateString('en-GB', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit'
+  });
+  const format_options : Intl.DateTimeFormatOptions = { 
+    hour: "2-digit", 
+    minute: "2-digit", 
+   };
+  const startTime = new Intl.DateTimeFormat("en-US", format_options).format(event.StartTime);
+  const endTime = new Intl.DateTimeFormat("en-US", format_options).format(event.EndTime);
+
+  // const startTime = new Intl.DateTimeFormat('en-US', format_options).format(event.StartTime)
+  // const endTime = new Intl.DateTimeFormat('en-US', format_options).format(event.EndTime)
+
   return {
     data: {
       type: "calendar_event",
       attributes: {
-        user_id: userId,
+        user_id: user_id,
         title: event.Subject,
         description: event.Description,
-        start_time: event.StartTime.toISOString(), // Ensure the date is in ISO format
-        end_time: event.EndTime.toISOString() // Ensure the date is in ISO format
+        start_date: startDate, // Ensure the date is in ISO format
+        end_date: endDate, // Ensure the date is in ISO format
+        start_time: startTime, // Ensure the date is in ISO format
+        end_time: endTime // Ensure the date is in ISO format
       }
     }
   };
@@ -74,7 +85,7 @@ function Calendar({ user }: Props) {
   };
   
   const current_token = localStorage.getItem('token');
-  const user_id = user.id
+  const user_id = user.data.id
 
   if (!current_token) {
   throw new Error("Token is null");
@@ -84,12 +95,14 @@ function Calendar({ user }: Props) {
     url: `https://rr-users-calendars-service-3e13398e3ea5.herokuapp.com/api/v1/users/${user_id}/calendar_events`,
     adaptor: new WebApiAdaptor,
     crossDomain:true,
-    headers: [{
-      Authorization: `${current_token}`
-      }]
+    headers: [
+      { Authorization: `${current_token}` },
+      { "Content-Type": "application/json" }
+      ]
     });
 
-  const save = 'e-save-icon e-icons';
+  const save_icon = 'e-save-icon e-icons';
+  const save_button = "e-schedule-dialog e-control e-btn e-lib e-primary e-event-save e-flat"
 
   // For future API call to delete CalendarEvent
   const delete_event = 'e-btn-icon e-icons e-delete-icon';
@@ -99,8 +112,7 @@ function Calendar({ user }: Props) {
     if (args.event && args.event.target) {
       const target = args.event.target as HTMLElement;
       const classNameSave = target.className;
-      console.log(classNameSave)
-        if (classNameSave === save) {
+        if (classNameSave === save_icon || save_button) {
           const newEvent: ScheduleEvent = {
             Id: scheduleData.length + 1,
             Subject: (args.data as any).Subject,
@@ -108,13 +120,27 @@ function Calendar({ user }: Props) {
             StartTime: new Date((args.data as any).StartTime),
             EndTime: new Date((args.data as any).EndTime),
           };
-          console.log(args.data)
           
-          setScheduleData([...scheduleData, newEvent]);
 
-          const apiFormattedEvent = transformToApiFormat(newEvent, user.id);
 
-          dataManager.insert(apiFormattedEvent)
+          // setScheduleData([...scheduleData, newEvent]);
+
+
+
+          const apiFormattedEvent = transformToApiFormat(newEvent, user_id);
+
+          try {
+            const apiResponse = async () => {
+              dataManager.insert(apiFormattedEvent)
+            }
+          } catch {
+            
+          }
+
+
+
+          console.log(scheduleData)
+
       }
     }
   };
