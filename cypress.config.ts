02efdba679b60a13e1abbb983ae98ed4e205ac4c
Asher Spurr <144856487+AsherSpurr@users.noreply.github.com>
2024-05-29 17:10:02 -0600

    testing with JWTs is weird

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    port: 3000,
   },
 
 });
--- cypress/e2e/landing-routes.cy.ts
+++ cypress/e2e/landing-routes.cy.ts
@@ -1,16 +1,22 @@
-// describe('Landing Page', () => {
-//     beforeEach(() => {
-//       cy.intercept('GET', 'https://8deefa6e-9aee-47e2-b8ea-a4dd591b3fc3.mock.pstmn.io/api/v1/users/1', {
-//         statusCode: 200,
-//         fixture: 'user'
-//       }).as('GetUser')
-//       .visit('http://localhost:3000/')
-//     })
+describe('Landing Page', () => {
+    beforeEach(() => {
+      cy.intercept('POST', 'https://rr-users-calendars-service-3e13398e3ea5.herokuapp.com/api/v1/users/login', {
+        statusCode: 200,
+        fixture: 'user'
+      }).as('GetUser')
+      .visit('http://localhost:3000/')
+      .get('.MuiPaper-root').first().within(() => {
+        cy.get('button').last().should('have.text', 'Sign In').click()
+      })
+      cy.get('input').eq(0).type('test25@test.com')
+      .get('input').eq(1).type('password')
+      .get('button').eq(0).click()
+    })
 
-//     it('Should navigate to Calender from Nav', () => {
-//         cy.get('a').eq(0).click()
-//         cy.url().should('include', '/user/1/calendar')
-//     })
+    it('Should navigate to Calender from Nav', () => {
+        cy.get('a').eq(0).click()
+        cy.url().should('include', '/user/1/calendar')
+    })
 
 //     it('Should navigate to Dashboard from Nav', () => {
 //         cy.get('a').eq(1).click()
@@ -61,4 +67,4 @@
 //     it('Should navigate to Article', () => {
 //           //Add tests after landing articles have been updated
 //     })
-// })
+})
