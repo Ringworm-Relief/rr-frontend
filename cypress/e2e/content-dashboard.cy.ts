e4d3070799ef23e43146e904f9d46ae5b6365651
Asher Spurr <144856487+AsherSpurr@users.noreply.github.com>
2024-06-03 13:59:10 -0600

    Update content-dashboard classnames

--- cypress/e2e/content-dashboard.cy.ts
+++ cypress/e2e/content-dashboard.cy.ts
@@ -51,20 +51,20 @@ describe('template spec', () => {
 
   it("Should include three pet cards", () => {
     //Pet cards should be visible
-    cy.get(".css-1v55mgz-MuiPaper-root-MuiCard-root").should("have.length", 3);
+    cy.get(".css-wlzvbk").should("have.length", 3);
 
-    cy.get(".css-1v55mgz-MuiPaper-root-MuiCard-root").eq(0).within(() => {
-      cy.get(".css-jyhipo-MuiCardMedia-root").should("have.attr", "style").should("include", 'background-image: url("/static/media/Pupper-profile.622f163b4e11a88c738ddd6ad62ca432.svg')
+    cy.get(".css-wlzvbk").eq(0).within(() => {
+      cy.get(".css-dtltaw").should("have.attr", "style").should("include", 'background-image: url("/static/media/Pupper-profile.622f163b4e11a88c738ddd6ad62ca432.svg')
       cy.get("h5").should("have.text", "keoki")
     })
 
-    cy.get(".css-1v55mgz-MuiPaper-root-MuiCard-root").eq(1).within(() => {
-      cy.get(".css-jyhipo-MuiCardMedia-root").should("have.attr", "style").should("include", 'background-image: url("/static/media/Kitty-profile.12d55bfd7fa95ad9ec585798bbb66f9f.svg')
+    cy.get(".css-wlzvbk").eq(1).within(() => {
+      cy.get(".css-dtltaw").should("have.attr", "style").should("include", 'background-image: url("/static/media/Kitty-profile.12d55bfd7fa95ad9ec585798bbb66f9f.svg')
       cy.get("h5").should("have.text", "koki")
     })
 
-    cy.get(".css-1v55mgz-MuiPaper-root-MuiCard-root").eq(2).within(() => {
-      cy.get(".css-1qvr50w-MuiTypography-root").should("have.text", "Add Pet")
+    cy.get(".css-wlzvbk").eq(2).within(() => {
+      cy.get(".css-1g3izzu").should("have.text", "Add Pet")
     })
   });
 
@@ -80,34 +80,34 @@ describe('template spec', () => {
     cy.get("a").eq(3).click();
 
     //Click Cleaning category
-    cy.get(".css-1uirrzm-MuiPaper-root-MuiCard-root").eq(0).click();
+    cy.get(".css-1g5t0ys").eq(0).click(); //Cleaning category ccard
     cy.url().should("include", "/education/cleaning");
 
     //Article card within cleaning category -> click save icon
-    cy.get(".css-174d5eo-MuiPaper-root-MuiCard-root").should("be.visible")
-    cy.get(".css-1t5hwt4-MuiButtonBase-root-MuiIconButton-root").click();
+    cy.get(".css-1qw96cp").should("be.visible")
+    cy.get(".css-1qlukr").click(); //Save button
 
     //Navigate back to Dashboard
     cy.get("a").eq(2).click();
 
     //Check if saved article is displayed
-    cy.get(".css-lfvik5-MuiPaper-root-MuiCard-root").within(() => {
+    cy.get(".css-1ez89u9").within(() => {
       cy.get("h6").should("have.text", "Cleaning 101")
-      cy.get(".css-1t5hwt4-MuiButtonBase-root-MuiIconButton-root").should("be.visible")
+      // cy.get(".css-1t5hwt4-MuiButtonBase-root-MuiIconButton-root").should("be.visible")
     })
   });
 
   it("Should show a link to manage account", () => {
     cy.get("a").eq(5).within(() => {
-      cy.get(".css-zg5e0d-MuiPaper-root-MuiCard-root")
-      cy.get(".css-1qvr50w-MuiTypography-root").should("have.text", "Manage Account")
+      cy.get(".css-wjqasx")
+      cy.get(".css-1g3izzu").should("have.text", "Manage Account")
     })
   });
 
   it("Should show a link to manage pets", () => {
     cy.get("a").eq(6).within(() => {
-      cy.get(".css-zg5e0d-MuiPaper-root-MuiCard-root")
-      cy.get(".css-1qvr50w-MuiTypography-root").should("have.text", "Manage Pets")
+      cy.get(".css-wjqasx")
+      cy.get(".css-1g3izzu").should("have.text", "Manage Pets")
     })
   });
 })
