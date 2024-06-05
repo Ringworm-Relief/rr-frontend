b9b9413ed9e21260413ec22529c2b26a9ced324a
Asher Spurr <144856487+AsherSpurr@users.noreply.github.com>
2024-06-05 14:07:32 -0600

    Adjust pet dashboard for mobile; fix floating footer

--- src/components/views/petDashboard/PetDashboard.tsx
+++ src/components/views/petDashboard/PetDashboard.tsx
@@ -1,144 +1,3 @@
-// import {
-//   Box,
-//   Card,
-//   CardContent,
-//   CardHeader,
-//   Grid,
-//   Typography,
-//   Divider
-// } from "@mui/material";
-// import Calendar from "../calendar/Calendar";
-// import Kitty from "../../../assets/Kitty-profile.svg";
-// import Pupper from "../../../assets/Pupper-profile.svg";
-// import { formateDate2 } from "../../../utils/interfaces"
-
-// interface Props {
-//   pet: any;
-//   user: any;
-//   pets: any[];
-// }
-
-// export default function PetDashboard({ pet, user, pets }: Props) {
-
-//   const style = {
-//     mr: 1,
-//     mt: 2,
-//     borderRadius: 3,
-//     boxShadow: "0px 5px 10px rgba(34, 35, 58, 0.1)",
-//     bottom: 100,
-//     left: -100,
-//     padding: 3,
-//     width: 350,
-//     height: 300,
-//     marginLeft: 0,
-//     overflow: "scroll",
-//     display: "flex",
-//     flexDirection: "column",
-//     alignItems: "center",
-//     color: "#9A352F",
-//     textAlign: "center"
-//   };
-
-//   const oralMeds = pet.medications.filter((med: any) => med.type === "Oral");
-//   const topicalMeds = pet.medications.filter(
-//     (med: any) => med.type === "Topical"
-//   );
-
-//   return (
-//     <div className="pet-dashboard">
-//       <Box
-//       sx={{
-//         backgroundImage:
-//         "linear-gradient(147deg, #fea2a25a 0%, #ffc4a44f 74%)",
-//       "&:after": {
-//         opacity: 0.5,
-//       },
-
-//       }}
-//       height="100vh"
-//       >
-//         <Box
-//           px={10}
-//           py={3}
-//           sx={{
-//             display: "flex",
-//             alignItems: "center",
-//             marginBottom: 2,
-//             backgroundImage:
-//               "linear-gradient(147deg, #fea2a25a 0%, #ffc4a44f 74%)",
-//             "&:after": {
-//               opacity: 0.5,
-//             },
-//           }}
-//         >
-//           <img
-//             className="pet-img"
-//             alt={pet.type === "Dog" ? "dog" : "cat"}
-//             src={pet.type === "Dog" ? Pupper : Kitty}
-//           />
-//           <Typography variant="h1" sx={{ fontSize: '75px', color: "#9A352F" }}>{pet.name}</Typography>
-//         </Box>
-//         <Grid px={10} container spacing={2} justifyContent="center" alignItems="center">
-//           <Grid item xs={12} sm={6} md={4}>
-//             <Card sx={style}>
-//               <CardHeader titleTypographyProps={{ fontSize: "1.8rem", fontWeight: "bold", padding: "0" }} title={`About ${pet.name}`} />
-//               <CardContent>
-//                 <Typography>Koki is a <strong>{pet.breed},</strong></Typography>
-//                 <Typography>born on <strong>{formateDate2(pet.birthday)}.</strong></Typography>
-//               </CardContent>
-//             </Card>
-//           </Grid>
-//           <Grid item xs={12} sm={6} md={4}>
-//             <Card sx={style}>
-//               <CardHeader titleTypographyProps={{ fontSize: "1.8rem", fontWeight: "bold" }} title="Diagnosis" />
-//               <CardContent>
-//                 <Typography>
-//                   <strong>Ringworm Type</strong>
-//                 </Typography>
-//                 <Typography>
-//                 {pet.ringworm?.ringworm_type || "N/A"}
-//                 </Typography>
-//                 <Divider sx={{ my: 2 }} />
-//                 <Typography>
-//                 <strong>Diagnosis Date</strong>
-//                 </Typography>
-//                 <Typography>
-//                 {formateDate2(pet.ringworm?.diagnosis_date) || "N/A"}
-//                 </Typography>
-//                 <Divider sx={{ my: 2 }} />
-//                 <Typography>
-//                 <strong>Symptoms</strong> {pet.ringworm.symptoms.map((symp: string) => {
-//                   return <Typography variant="body2">- {symp}</Typography>
-//                 }) || "N/A"}
-//                 </Typography>
-//               </CardContent>
-//             </Card>
-//           </Grid>
-//           <Grid item xs={12} sm={6} md={4}>
-//             <Card sx={style}>
-//               <CardHeader titleTypographyProps={{ fontSize: "1.8rem", fontWeight: "bold" }} title="Medications" />
-//               <CardContent>
-//                 <Typography variant="h6" marginBottom="5px">Oral</Typography>
-//                 {oralMeds.map((med: any) => (
-//                   <Typography variant="body1" key={med.name}>
-//                   - {med.name}: {med.dosage}
-//                   </Typography>
-//                 ))}
-//                 <Divider sx={{ my: 2 }} />
-//                 <Typography variant="h6" marginBottom="5px">Topical</Typography>
-//                 {topicalMeds.map((med: any) => (
-//                   <Typography variant="body1" key={med.name}>
-//                    - {med.name}: {med.dosage}, {med.frequency}
-//                   </Typography>
-//                 ))}
-//               </CardContent>
-//             </Card>
-//           </Grid>
-//         </Grid>
-//       </Box>
-//     </div>
-//   );
-// }
 
 import {
   Box,
@@ -161,19 +20,18 @@ interface Props {
 }
 
 export default function PetDashboard({ pet, user, pets }: Props) {
-  // console.log("pet dash pet", pet)
+
   const style = {
     mr: 1,
-    mt: 2,
+    mt: 4,
     borderRadius: 3,
     boxShadow: "0px 5px 10px rgba(34, 35, 58, 0.1)",
     bottom: 100,
     left: -100,
     padding: 3,
-    width: 350,
     height: 300,
     marginLeft: 0,
-    overflow: "scroll",
+    overflow: "hide",
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
@@ -194,10 +52,10 @@ export default function PetDashboard({ pet, user, pets }: Props) {
             "linear-gradient(147deg, #fea2a25a 0%, #ffc4a44f 74%)",
           "&:after": { opacity: 0.5 },
         }}
-        height="100vh"
+        minHeight="100vh"
       >
         <Box
-          px={10}
+          px={window.innerWidth <= 500 ? 3 : 10}
           py={3}
           sx={{
             display: "flex",
@@ -213,7 +71,7 @@ export default function PetDashboard({ pet, user, pets }: Props) {
           </Typography>
         </Box>
         <Grid
-          px={10}
+          px={window.innerWidth <= 780 ? 1 : 10}
           container
           spacing={2}
           justifyContent="center"
