67884474cd10c166f7a8d2b4008781fae2b9d781
Pareesa Kamgar Dayhoff <pareesakd1118@gmail.com>
2024-06-05 20:55:44 -0700

    Ok

--- src/apiCalls/petApiCalls.ts
+++ src/apiCalls/petApiCalls.ts
@@ -25,16 +25,22 @@ function putPet(pet: any, id: any) {
 function putRingworm(ringworm: any) {
     return fetch(`https://8deefa6e-9aee-47e2-b8ea-a4dd591b3fc3.mock.pstmn.io/api/v1/pet_ringworms/${ringworm.id}`, {
         method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
+        headers: { 
+            'Content-Type': 'application/json',
+            "Authorization": `${localStorage.getItem('token')}` 
+         },
         body: JSON.stringify(ringworm)
     })
     .then(res => res.json());
 }
 
-function putMedication(medication: any) {
-    return fetch(`https://8deefa6e-9aee-47e2-b8ea-a4dd591b3fc3.mock.pstmn.io/api/v1/pet_medications/${medication.id}`, {
+function putMedications(medication: any, id: any) {
+    return fetch(`https://user-pets-service-4a1c97bde8d0.herokuapp.com/api/v1/pets/${id}`, {
         method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
+        headers: { 
+            'Content-Type': 'application/json',
+            "Authorization": `${localStorage.getItem('token')}` 
+         },
         body: JSON.stringify(medication)
     })
     .then(res => res.json());
