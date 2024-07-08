67c8176366f5c7a5863085835f658bba40aeb84f
Pareesa Kamgar Dayhoff <pareesakd1118@gmail.com>
2024-07-07 23:34:13 -0700

    Delete threads

--- src/apiCalls/forumApiCalls.ts
+++ src/apiCalls/forumApiCalls.ts
@@ -12,4 +12,13 @@ function postThread(thread: any) {
     })
 }
 
-export { getThreads, postThread }
+function deleteThread(id: number | null) {
+    return fetch(`https://ringworm-forum-0c0291e817b7.herokuapp.com/threads/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+}
+
+export { getThreads, postThread, deleteThread }
