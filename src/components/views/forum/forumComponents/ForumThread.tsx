deef5f9b330df9f25414127868d5757c7a607594
Pareesa Kamgar Dayhoff <pareesakd1118@gmail.com>
2024-07-09 13:12:26 -0700

    Up votes and down votes on threads and comments on second page

--- src/components/views/forum/forumComponents/ForumThread.tsx
+++ src/components/views/forum/forumComponents/ForumThread.tsx
@@ -1,5 +1,3 @@
-
-
 import React, { useState, useEffect } from "react";
 import {
   Typography,
@@ -10,12 +8,17 @@ import {
   CardContent,
   Divider,
   TextField,
-  Stack
 } from "@mui/material";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import ThumbDownAltIcon from "@mui/icons-material/ThumbDownAlt";
-import ThumbUpAltIcon from "@mui/icons-material/ThumbUpAlt";
-import { getSingleThread, postPost, deletePost } from "../../../../apiCalls/forumApiCalls";
+import ThumbDownOffAltIcon from "@mui/icons-material/ThumbDownOffAlt";
+import ThumbUpOffAltIcon from "@mui/icons-material/ThumbUpOffAlt";
+import {
+  getSingleThread,
+  postPost,
+  deletePost,
+  postThread,
+  updatePostVotes,
+} from "../../../../apiCalls/forumApiCalls";
 import { useParams } from "react-router-dom";
 import SendIcon from "@mui/icons-material/Send";
 import DeleteIcon from "@mui/icons-material/Delete";
@@ -27,6 +30,16 @@ interface Props {
 export default function ForumThread({ user }: Props) {
   const [thread, setThread] = useState<any>({});
   const [posts, setPosts] = useState<any[]>([]);
+  const [userVotes, setUserVotes] = useState<{
+    [key: string]: { up_votes: boolean | null; down_votes: boolean | null };
+  }>({});
+  const [threadVote, setThreadVote] = useState<{
+    up_votes: boolean;
+    down_votes: boolean;
+  }>({
+    up_votes: false,
+    down_votes: false,
+  });
   const { category, id } = useParams();
   const [newPost, setNewPost] = useState<any>({
     content: "",
@@ -48,6 +61,11 @@ export default function ForumThread({ user }: Props) {
       .then((data) => {
         setThread(data[0]);
         setPosts(data[0].posts);
+        const initialVotes = data[0].posts.reduce((acc: any, post: any) => {
+          acc[post.id] = { up_votes: false, down_votes: false };
+          return acc;
+        }, {});
+        setUserVotes(initialVotes);
       })
       .catch((error) => {
         console.error("Error adding new thread:", error);
@@ -56,35 +74,24 @@ export default function ForumThread({ user }: Props) {
 
   const handleDeletePost = (postId: string) => {
     deletePost(postId)
-    .then((response) => {
-        if (!response.ok) {
-            throw new Error("Error deleting post")
-        }
-        return response.json()
-    })
-    .then(() => {
-        getSingleThread(category, id)
       .then((response) => {
         if (!response.ok) {
-          throw new Error("Network response was not ok");
+          throw new Error("Error deleting post");
         }
         return response.json();
       })
-      .then((data) => {
-        setThread(data[0]);
-        setPosts(data[0].posts);
+      .then(() => {
+        displayThread();
       })
       .catch((error) => {
-        console.error("Error adding new thread:", error);
+        console.error("Error deleting post:", error);
       });
-    })
-  }
+  };
 
   useEffect(() => {
     displayThread();
-  }, []);
+  }, [user]);
 
-  console.log("possssst", posts)
   const handleSubmitPost = () => {
     postPost(newPost, id)
       .then((response) => {
@@ -93,13 +100,8 @@ export default function ForumThread({ user }: Props) {
         }
         return response.json();
       })
-      .then((addedPost) => {
-        return getSingleThread(category, id);
-      })
-      .then((updatedThread) => updatedThread.json())
-      .then((data) => {
-        setThread(data[0]);
-        setPosts(data[0].posts);
+      .then(() => displayThread())
+      .then(() => {
         setNewPost({
           content: "",
           user_id: user.data.id,
@@ -112,6 +114,146 @@ export default function ForumThread({ user }: Props) {
       .catch((error) => {
         console.error("Error adding new thread:", error);
       });
+  };
+
+  const handleThreadUpVote = () => {
+    if (!threadVote.up_votes && !threadVote.down_votes) {
+      const updatedThread = { ...thread, up_votes: thread.up_votes + 1 };
+
+      postThread(updatedThread)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then(() => {
+          setThreadVote({ up_votes: true, down_votes: false });
+          getSingleThread(category, id)
+            .then((response) => {
+              if (!response.ok) {
+                throw new Error("Network response was not ok");
+              }
+              return response.json();
+            })
+            .then((data) => {
+              setThread(data[0]);
+              setPosts(data[0].posts);
+            })
+            .catch((error) => {
+              console.error("Error adding new thread:", error);
+            });
+        });
+    }
+  };
+
+  const handleThreadDownVote = () => {
+    if (!threadVote.up_votes && !threadVote.down_votes) {
+      const updatedThread = { ...thread, down_votes: thread.down_votes + 1 };
+
+      postThread(updatedThread)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then(() => {
+          setThreadVote({ up_votes: false, down_votes: true });
+
+          getSingleThread(category, id)
+            .then((response) => {
+              if (!response.ok) {
+                throw new Error("Network response was not ok");
+              }
+              return response.json();
+            })
+            .then((data) => {
+              setThread(data[0]);
+              setPosts(data[0].posts);
+            })
+            .catch((error) => {
+              console.error("Error adding new thread:", error);
+            });
+        });
+    }
+  };
+
+  const handleUpVote = (postId: string) => {
+    if (!userVotes[postId]?.up_votes && !userVotes[postId]?.down_votes) {
+      const updatedPost = posts.find((post) => post.id === postId);
+      const newObject = { ...updatedPost, up_votes: updatedPost.up_votes + 1 };
+      updatePostVotes(postId, newObject)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then(() => {
+          setUserVotes((prevVotes) => ({
+            ...prevVotes,
+            [postId]: { down_votes: false, up_votes: true },
+          }));
+          getSingleThread(category, id)
+            .then((response) => {
+              if (!response.ok) {
+                throw new Error("Network response was not ok");
+              }
+              return response.json();
+            })
+            .then((data) => {
+              setThread(data[0]);
+              setPosts(data[0].posts);
+            })
+            .catch((error) => {
+              console.error("Error adding new thread:", error);
+            });
+        })
+        .catch((error) => {
+          console.error("Error updating post votes:", error);
+        });
+    }
+  };
+
+  const handleDownVote = (postId: string) => {
+    if (!userVotes[postId]?.up_votes && !userVotes[postId]?.down_votes) {
+      const updatedPost = posts.find((post) => post.id === postId);
+      const newObject = {
+        ...updatedPost,
+        down_votes: updatedPost.down_votes + 1,
+      };
+      updatePostVotes(postId, newObject)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then(() => {
+          setUserVotes((prevVotes) => ({
+            ...prevVotes,
+            [postId]: { down_votes: true, up_votes: false },
+          }));
+          getSingleThread(category, id)
+            .then((response) => {
+              if (!response.ok) {
+                throw new Error("Network response was not ok");
+              }
+              return response.json();
+            })
+            .then((data) => {
+              setThread(data[0]);
+              setPosts(data[0].posts);
+            })
+            .catch((error) => {
+              console.error("Error adding new thread:", error);
+            });
+        })
+        .catch((error) => {
+          console.error("Error updating post votes:", error);
+        });
+    }
   };
 
   return (
@@ -129,7 +271,9 @@ export default function ForumThread({ user }: Props) {
               sx={{ fontSize: 60, mr: 1 }}
             />
             <Box ml={2}>
-              <Typography variant="body2"><strong>{`${thread.first_name} ${thread.last_name}`}</strong></Typography>
+              <Typography variant="body2">
+                <strong>{`${thread.first_name} ${thread.last_name}`}</strong>
+              </Typography>
               <Typography variant="body2" color="text.secondary">
                 Posted{" "}
                 {new Date(thread.created_at).toLocaleDateString("en-US", {
@@ -141,11 +285,23 @@ export default function ForumThread({ user }: Props) {
             </Box>
           </Box>
         </CardContent>
-        <Box display="flex" flexDirection="row" sx={{ position: "absolute", left: 1040, top: 20 }}>
+        <Box
+          display="flex"
+          flexDirection="row"
+          sx={{ position: "absolute", left: 1040, top: 20 }}
+        >
           <Typography>{thread.up_votes}</Typography>
-          <ThumbUpAltIcon sx={{ mx: 0.5 }} />
+          <ThumbUpOffAltIcon
+            sx={{ mx: 0.5, cursor: "pointer" }}
+            color={threadVote.up_votes ? "primary" : "inherit"}
+            onClick={handleThreadUpVote}
+          />
           <Typography>{thread.down_votes}</Typography>
-          <ThumbDownAltIcon sx={{ ml: 0.5 }} />
+          <ThumbDownOffAltIcon
+            sx={{ mx: 0.5, cursor: "pointer" }}
+            color={threadVote.down_votes ? "primary" : "inherit"}
+            onClick={handleThreadDownVote}
+          />
         </Box>
       </Card>
 
@@ -172,13 +328,35 @@ export default function ForumThread({ user }: Props) {
                 color="primary"
                 sx={{ fontSize: 50 }}
               />
-              <Box display="flex" flexDirection="row" sx={{ position: "absolute", left: 1030, bottom: 30 }}>
+              <Box
+                display="flex"
+                flexDirection="row"
+                sx={{ position: "absolute", left: 1030, bottom: 30 }}
+              >
                 <Typography>{post.up_votes}</Typography>
-                <ThumbUpAltIcon sx={{ mx: 0.5 }} />
+                <ThumbUpOffAltIcon
+                  sx={{ mx: 0.5, cursor: "pointer" }}
+                  color={userVotes[post.id].up_votes ? "primary" : "inherit"}
+                  onClick={() => handleUpVote(post.id)}
+                />
                 <Typography>{post.down_votes}</Typography>
-                <ThumbDownAltIcon sx={{ ml: 0.5 }} />
+                <ThumbDownOffAltIcon
+                  sx={{ ml: 0.5, cursor: "pointer" }}
+                  color={userVotes[post.id].down_votes ? "primary" : "inherit"}
+                  onClick={() => handleDownVote(post.id)}
+                />
               </Box>
-              {post.user_id === user.data.id && <DeleteIcon onClick={() => handleDeletePost(post.id)}sx={{ position: "absolute", left: 1000, bottom: 30, cursor: "pointer" }} />}
+              {post.user_id === user.data.id && (
+                <DeleteIcon
+                  onClick={() => handleDeletePost(post.id)}
+                  sx={{
+                    position: "absolute",
+                    left: 1000,
+                    bottom: 30,
+                    cursor: "pointer",
+                  }}
+                />
+              )}
               <Box
                 sx={{
                   position: "absolute",
@@ -193,8 +371,9 @@ export default function ForumThread({ user }: Props) {
               />
             </Box>
             <Box>
-            
-              <Typography variant="body2"><strong>{`${post.first_name} ${post.last_name}`}</strong></Typography>
+              <Typography variant="body2">
+                <strong>{`${post.first_name} ${post.last_name}`}</strong>
+              </Typography>
               <Typography variant="body2" color="text.secondary">
                 Posted{" "}
                 {new Date(post.created_at).toLocaleDateString("en-US", {
@@ -206,7 +385,6 @@ export default function ForumThread({ user }: Props) {
               <Typography variant="body1" sx={{ mt: 2 }}>
                 {post.content}
               </Typography>
-             
             </Box>
           </CardContent>
         </Card>
@@ -214,7 +392,7 @@ export default function ForumThread({ user }: Props) {
 
       <Divider sx={{ my: 2 }} />
 
-       <Box
+      <Box
         sx={{
           p: 2,
           pb: 8,
@@ -234,7 +412,9 @@ export default function ForumThread({ user }: Props) {
             rows={4}
             variant="outlined"
             value={newPost.content}
-            onChange={(e) => setNewPost({ ...newPost, content: e.target.value })}
+            onChange={(e) =>
+              setNewPost({ ...newPost, content: e.target.value })
+            }
           />
         </Box>
         <Button
@@ -249,4 +429,3 @@ export default function ForumThread({ user }: Props) {
     </Container>
   );
 }
-
